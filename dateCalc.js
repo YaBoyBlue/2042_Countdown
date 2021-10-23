3740c57f863ef1225fddfd1a458dac131f2ac37a
YaBoyBlue <92649461+YaBoyBlue@users.noreply.github.com>
2021-10-23 16:10:21 -0500

    Push

--- dateCalc.js
+++ dateCalc.js
@@ -1,6 +1,6 @@
 function getDateDiff(date, timezone) {
 
-    let dateDifference = date - new Date().getTime()
+    let dateDifference = (date - new Date().getTime()) + ((-timezone) * (1000 * 60))
 
     let days = Math.floor(dateDifference / (1000 * 60 * 60 * 24))
     let hours = Math.floor((dateDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -14,19 +14,19 @@ setInterval(() => {
 
     const timeZone = new Date().getTimezoneOffset()
 
-    document.getElementById("ctimezone").innerHTML = "UTC" + -1 * timeZone / 60
+    document.getElementById("ctimezone").innerHTML = "UTC" + ((-timeZone) / 60)
 
     const elementA = document.getElementById("diffRelease")
     const elementB = document.getElementById("diffOrdered")
     const elementC = document.getElementById("diffPreLoad")
 
-    const dateA = new Date("Novemeber 19, 2021 12:00:00")
-    const dateB = new Date("Novemeber 12, 2021 12:00:00")
-    const dateC = new Date("Novemeber 10, 2021 12:00:00")
+    const dateA = new Date("Novemeber 19, 2021 17:00:00")
+    const dateB = new Date("Novemeber 12, 2021 17:00:00")
+    const dateC = new Date("Novemeber 10, 2021 17:00:00")
 
-    let diffA = getDateDiff(dateA)
-    let diffB = getDateDiff(dateB)
-    let diffC = getDateDiff(dateC)
+    let diffA = getDateDiff(dateA, timeZone)
+    let diffB = getDateDiff(dateB, timeZone)
+    let diffC = getDateDiff(dateC, timeZone)
 
     elementA.innerHTML = `${diffA[0]} Days, ${diffA[1]} Hours, ${diffA[2]} Minutes, ${diffA[3]} Seconds.`
     elementB.innerHTML = `${diffB[0]} Days, ${diffB[1]} Hours, ${diffB[2]} Minutes, ${diffB[3]} Seconds.`
