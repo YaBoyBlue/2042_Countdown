9447cb12ea77f7521521ac38e4066a88b75865c1
YaBoyBlue <92649461+YaBoyBlue@users.noreply.github.com>
2021-10-24 00:23:33 -0500

    Update

--- dateCalc.js
+++ dateCalc.js
@@ -1,5 +1,3 @@
-let timeZone = new Date().getTimezoneOffset()
-
 const elementT = document.getElementById("ctimezone")
 const elementA = document.getElementById("diffRelease")
 const elementB = document.getElementById("diffOrdered")
@@ -7,7 +5,9 @@ const elementC = document.getElementById("diffPreLoad")
 
 const dateA = new Date("Novemeber 19, 2021 12:00:00")
 const dateB = new Date("Novemeber 12, 2021 12:00:00")
-const dateC = new Date("Novemeber 10, 2021 12:00:00").getTime() - timeZone
+const dateC = new Date("Novemeber 10, 2021 12:00:00")
+
+const timeZone = new Date().getTimezoneOffset()
 
 if (timeZone >= 0) {
     elementT.innerHTML = "UTC " + ((-timeZone) / 60)
