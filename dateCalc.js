8c42c95f5050d7f252403830832e06f285fc51fa
YaBoyBlue <92649461+YaBoyBlue@users.noreply.github.com>
2021-10-23 21:08:25 -0500

    Push

--- dateCalc.js
+++ dateCalc.js
@@ -1,4 +1,4 @@
-const timeZone = new Date().getTimezoneOffset()
+let timeZone = new Date().getTimezoneOffset()
 
 const elementT = document.getElementById("ctimezone")
 const elementA = document.getElementById("diffRelease")
@@ -9,37 +9,39 @@ const dateA = new Date("Novemeber 19, 2021 17:00:00")
 const dateB = new Date("Novemeber 12, 2021 17:00:00")
 const dateC = new Date("Novemeber 10, 2021 17:00:00")
 
-if (timeZone > 0) {
-    elementT.innerHTML = " UTC " + ((-timeZone) / 60)
-} else {
-    elementT.innerHTML = " UTC +" + ((-timeZone) / 60)
-}
+// if (timeZone > 0) {
+//     elementT.innerHTML = timeZone + " UTC " + ((-timeZone) / 60) + " " +  - (timeZone * 1000 * 60)
+// } else {
+//     elementT.innerHTML = timeZone + " UTC +" + ((-timeZone) / 60) + " " +  - (timeZone * 1000 * 60)
+// }
 
-function getDateDiff(date, timezone) {
+function getDHMS(timeDes, timeZssone) {
 
-    let dateDifference = date - (new Date().getTime() + (-timezone * 1000 * 60))
+    //const timeDesired = timeDes.getTime()
+    const timeUTC = new Date().getTime() + timeZone * 60 * 1000
+
+    let dateDifference = timeDes - timeUTC
+
+    elementT.innerHTML = timeZone / 60
 
     let days = Math.floor(dateDifference / (1000 * 60 * 60 * 24))
     let hours = Math.floor((dateDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     let minutes = Math.floor((dateDifference % (1000 * 60 * 60)) / (1000 * 60))
     let seconds = Math.floor((dateDifference % (1000 * 60)) / 1000)
 
-    // if (timeZone > 0) {
-    //     elementT.innerHTML = timeZone + " UTC " + ((-timeZone) / 60) + " " + ((-timeZone) * (1000 * 60)) + " " + hours
-    // } else {
-    //     elementT.innerHTML = timeZone + " UTC +" + ((-timeZone) / 60) + " " + ((-timeZone) * (1000 * 60)) + " " + hours
-    // }
-
     return [days, hours, minutes, seconds]
 }
 
-setInterval(() => {
-
-    let diffA = getDateDiff(dateA, timeZone)
-    let diffB = getDateDiff(dateB, timeZone)
-    let diffC = getDateDiff(dateC, timeZone)
+function getTime() {
+    let diffA = getDHMS(dateA, timeZone)
+    let diffB = getDHMS(dateB, timeZone)
+    let diffC = getDHMS(dateC, timeZone)
 
     elementA.innerHTML = `${diffA[0]} Days, ${diffA[1]} Hours, ${diffA[2]} Minutes, ${diffA[3]} Seconds.`
     elementB.innerHTML = `${diffB[0]} Days, ${diffB[1]} Hours, ${diffB[2]} Minutes, ${diffB[3]} Seconds.`
     elementC.innerHTML = `${diffC[0]} Days, ${diffC[1]} Hours, ${diffC[2]} Minutes, ${diffC[3]} Seconds.`
-}, 1000)
+}
+
+getTime()
+
+setInterval(getTime, 1000)
